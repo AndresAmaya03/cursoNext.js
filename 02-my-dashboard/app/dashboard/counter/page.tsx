d159c298021a263ab214d0fb57e50885dfbef686
Andres Amaya <andresrobertoamayabracamonte@gmail.com>
2025-01-03 00:12:33 -0700

    seccion 2.- server/client side rendering

---
+++ 02-my-dashboard/app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+import { CartCounter } from "@/app/shopping-cart";
+import { Metadata } from "next";
+
+
+export const metadata:Metadata = {
+ title: 'Shopping cart',
+ description: 'Counter',
+};
+
+export default function CounterPage() {
+
+
+
+  return (
+    <div className="flex flex-col items-center justify-center text-black w-full h-full">
+      <span>Products</span>
+      <CartCounter value={20}/>
+    </div>
+  );
+}
---
+++ 02-my-dashboard/app/page.tsx
@@ -0,0 +1,12 @@
+import { redirect } from "next/navigation";
+
+export default function Home() {
+
+  redirect('/dashboard/main')
+
+  return (
+    <>
+      <h1>hola mundo</h1>
+    </>
+  );
+}
